2c9654983a2a4459b4170a29a3c4dfede888a6f3
DmitryScaletta <dmitryscaletta@mail.ru>
2025-03-24 21:08:13 +0300

    🔨 Refactor generate types script

--- scripts/gen.ts
+++ scripts/gen.ts
@@ -1,17 +1,7 @@
-import path from 'node:path';
 import fsp from 'node:fs/promises';
 import { type TSchema } from '@sinclair/typebox';
 import { compile } from 'json-schema-to-typescript';
-
-const getSchemaPaths = async () => {
-  const files = await fsp.readdir('src/queries', { recursive: true });
-  const schemaFiles = files
-    .filter((filePath) => filePath.endsWith('schema.ts'))
-    .sort();
-  return schemaFiles.map(
-    (filePath) => `../src/queries/${filePath.replace(path.sep, '/')}`,
-  );
-};
+import { getQueriesMetadata } from './utils/getQueriesMetadata.ts';
 
 const replaceRefsWithIds = (schema: TSchema) => {
   for (const value of Object.values(schema)) {
@@ -44,45 +34,30 @@ const jsonSchemaToTs = (schema: TSchema) =>
   );
 
 export const gen = async () => {
-  const schemaPaths = await getSchemaPaths();
-  const schemaImports: Record<string, TSchema>[] = await Promise.all(
-    schemaPaths.map((schemaPath) => import(schemaPath)),
-  );
+  const queriesMetadata = await getQueriesMetadata();
   const tsSchemas: TSchema[] = [];
-  const queries: [name: string, displayName: string][] = [];
-
-  for (const [i, schemas] of Object.entries(schemaImports)) {
-    for (const [schemaName, schema] of Object.entries(schemas)) {
-      if (
-        !schemaName.endsWith('Schema') ||
-        !schema.$id ||
-        ['VariablesSchema', 'DataSchema', 'ResponseSchema'].includes(schemaName)
-      ) {
-        continue;
-      }
-      tsSchemas.push(schema as TSchema);
-    }
-    if ('VariablesSchema' in schemas) {
-      tsSchemas.push(schemas.VariablesSchema);
-    }
-    if ('DataSchema' in schemas) {
-      const name = schemaPaths[i as any].split('/').at(-2)!;
-      const displayName = schemas.DataSchema.$id!.replace(/Data$/, '');
-      queries.push([name, displayName]);
-      tsSchemas.push(schemas.DataSchema);
-    }
-  }
-
-  const tsDefs = await Promise.all(tsSchemas.map(jsonSchemaToTs));
   const responseTypes: string[] = [];
   const queryResponseMap: string[] = [];
-  for (const [name, displayName] of queries) {
+
+  for (const {
+    name,
+    displayName,
+    additionalSchemas,
+    VariablesSchema,
+    DataSchema,
+  } of queriesMetadata) {
+    tsSchemas.push(
+      ...additionalSchemas.filter((s) => s.$id),
+      VariablesSchema,
+      DataSchema,
+    );
     responseTypes.push(
       `export type ${displayName}Response = QueryResponse<${displayName}Data, '${name}'>;`,
     );
     queryResponseMap.push(`  ${name}: ${displayName}Response;`);
   }
-  queryResponseMap.push();
+
+  const tsDefs = await Promise.all(tsSchemas.map(jsonSchemaToTs));
   let ts = [
     "import type { QueryResponse } from '../types.ts';\n",
     ...tsDefs,
