e8dbe8b759ef9b2e2272e4eaa69b503403337ace
DmitryScaletta <dmitryscaletta@mail.ru>
2025-03-24 18:54:49 +0300

    🔨 Add swagger script

---
+++ scripts/swagger.ts
@@ -0,0 +1,141 @@
+import fsp from 'node:fs/promises';
+import type { TSchema } from '@sinclair/typebox';
+import { getQueriesMetadata } from './utils/getQueriesMetadata.ts';
+
+const template: any = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Twitch GraphQL API Queries',
+    description:
+      '[github.com/DmitryScaletta/twitch-gql-queries](https://github.com/DmitryScaletta/twitch-gql-queries)',
+    version: 'GraphQL',
+  },
+  servers: [{ url: 'https://gql.twitch.tv/gql', description: 'GraphQL' }],
+  security: [],
+  tags: [],
+  paths: {},
+  components: {
+    schemas: {},
+  },
+};
+
+const normalizeRefs = (schema: TSchema) => {
+  for (const value of Object.values(schema)) {
+    if (typeof value === 'object' && value !== null) {
+      if (value.$ref) {
+        if (!value.$ref.startsWith('#')) {
+          value.$ref = `#/components/schemas/${value.$ref}`;
+        }
+      } else {
+        normalizeRefs(value);
+      }
+    }
+  }
+  return schema;
+};
+
+const buildOpenApi = async () => {
+  const queriesMetadata = await getQueriesMetadata();
+
+  const openApi = template;
+
+  for (const {
+    name,
+    displayName,
+    description,
+    tags,
+    requestBodies,
+    responseExamples,
+    additionalSchemas,
+    VariablesSchema,
+    RequestBodySchema,
+    DataSchema,
+    ResponseSchema,
+  } of queriesMetadata) {
+    const endpointPath = `#${name}`;
+    let reqBodyExample = undefined;
+    let reqBodyExamples = undefined;
+
+    if (requestBodies.length > 0) {
+      reqBodyExamples = {} as any;
+      for (const { summary, value } of requestBodies) {
+        reqBodyExamples[summary] = { value };
+      }
+    } else {
+      reqBodyExample = requestBodies[0];
+    }
+
+    let resExample = undefined;
+    let resExamples = undefined;
+
+    if (responseExamples.length > 0) {
+      resExamples = {} as any;
+      for (const { summary, value } of responseExamples) {
+        resExamples[summary] = { value };
+      }
+    } else {
+      resExample = responseExamples[0];
+    }
+
+    openApi.paths[endpointPath] = {
+      post: {
+        summary: '',
+        tags,
+        description,
+        parameters: [
+          {
+            in: 'header',
+            name: 'Client-ID',
+            required: true,
+            default: 'kimne78kx3ncx6brgo4mv6wki5h1ko',
+          },
+        ],
+        requestBody: {
+          description: '',
+          required: true,
+          content: {
+            'application/json': {
+              schema: {
+                $ref: `#/components/schemas/${displayName}RequestBody`,
+              },
+              example: reqBodyExample,
+              examples: reqBodyExamples,
+            },
+          },
+        },
+        responses: {
+          '200': {
+            description: '',
+            content: {
+              'application/json': {
+                schema: {
+                  $ref: `#/components/schemas/${displayName}Response`,
+                },
+                example: resExample,
+                examples: resExamples,
+              },
+            },
+          },
+        },
+      },
+    };
+
+    ResponseSchema.$id = `${displayName}Response`;
+
+    for (const schema of [
+      ...additionalSchemas,
+      VariablesSchema,
+      RequestBodySchema,
+      DataSchema,
+      ResponseSchema,
+    ]) {
+      if (!schema.$id) continue;
+      openApi.components.schemas[schema.$id.replace(/Schema$/, '')] =
+        normalizeRefs(schema);
+    }
+  }
+
+  await fsp.writeFile('openapi.json', JSON.stringify(openApi, null, 2));
+};
+
+buildOpenApi();
---
+++ scripts/utils/getQueriesMetadata.ts
@@ -0,0 +1,154 @@
+import path from 'node:path';
+import assert from 'node:assert';
+import fsp from 'node:fs/promises';
+import { Type as T, type TSchema } from '@sinclair/typebox';
+import { strictObject } from '../../src/schema.ts';
+
+type RequestBody = {
+  summary: string;
+  value: any;
+};
+type ResponseExample = {
+  summary: string;
+  value: any;
+};
+type QueryMetadata = {
+  name: string;
+  displayName: string;
+  description?: string;
+  tags: string[];
+  requestBodies: RequestBody[];
+  responseExamples: ResponseExample[];
+  additionalSchemas: TSchema[];
+  VariablesSchema: TSchema;
+  RequestBodySchema: TSchema;
+  DataSchema: TSchema;
+  ResponseSchema: TSchema;
+};
+
+const REQUIRED_EXPORTS = [
+  'name',
+  'displayName',
+  'tags',
+  'VariablesSchema',
+  'DataSchema',
+  'ResponseSchema',
+];
+
+const createRequestBodySchema = (
+  queryName: string,
+  displayQueryName: string,
+  variables: TSchema,
+  sha256Hash: string,
+) =>
+  strictObject(
+    {
+      operationName: T.Literal(queryName),
+      variables,
+      extensions: strictObject({
+        persistedQuery: strictObject({
+          version: T.Literal(1),
+          sha256Hash: T.Literal(sha256Hash),
+        }),
+      }),
+    },
+    { $id: `${displayQueryName}RequestBody` },
+  );
+
+export const getQueriesMetadata = async () => {
+  let filePaths = await fsp.readdir('src/queries', { recursive: true });
+  filePaths = filePaths.map((f) => f.replaceAll(path.sep, '/')).sort();
+  const schemaFilePaths = filePaths
+    .filter((f) => f.endsWith('/schema.ts'))
+    .map((f) => `../../src/queries/${f}`);
+  const httpFilePaths = filePaths
+    .filter((f) => f.endsWith('/query.http'))
+    .map((f) => `./src/queries/${f}`);
+  const responsesFilePaths = filePaths
+    .filter((f) => f.includes('/mocks/'))
+    .map((f) => `./src/queries/${f}`);
+
+  const [schemaImports, httpFiles, responseFiles] = await Promise.all([
+    Promise.all(schemaFilePaths.map((f) => import(f))),
+    Promise.all(httpFilePaths.map((f) => fsp.readFile(f, 'utf-8'))),
+    Promise.all(responsesFilePaths.map((f) => fsp.readFile(f, 'utf-8'))),
+  ]);
+  assert(schemaImports.length === httpFiles.length);
+
+  const queries: QueryMetadata[] = [];
+  for (let i = 0; i < schemaImports.length; i += 1) {
+    const schemaImport = schemaImports[i];
+    const httpFile = httpFiles[i];
+
+    for (const exportName of REQUIRED_EXPORTS) {
+      assert(
+        exportName in schemaImport,
+        `${exportName} not found in ${schemaFilePaths[i]}`,
+      );
+    }
+
+    const additionalSchemas: TSchema[] = [];
+    for (const [objName, obj] of Object.entries(schemaImport)) {
+      if (REQUIRED_EXPORTS.includes(objName) || !objName.endsWith('Schema')) {
+        continue;
+      }
+      additionalSchemas.push(obj as TSchema);
+    }
+
+    const requestBodies: RequestBody[] = [];
+    const requestBodyArr = httpFile.split(/### ([^\n]+)/).slice(1);
+    for (let j = 0; j < requestBodyArr.length; j += 2) {
+      const content = requestBodyArr[j + 1].match(/\n(?<requestBody>{.+\n})/s)
+        ?.groups?.requestBody;
+      assert(content, `No request body found in ${httpFilePaths[i]}`);
+      requestBodies.push({
+        summary: requestBodyArr[j],
+        value: JSON.parse(content),
+      });
+    }
+    assert(
+      requestBodies.length > 0,
+      `No request body found in ${httpFilePaths[i]}`,
+    );
+    const { sha256Hash } = requestBodies[0].value.extensions.persistedQuery;
+    assert(sha256Hash);
+
+    const responseExamples: ResponseExample[] = [];
+    for (let i = 0; i < responseFiles.length; i += 1) {
+      const responseFilePath = responsesFilePaths[i];
+      const responseFile = responseFiles[i];
+      if (!responseFilePath.includes(schemaImport.name)) continue;
+      responseExamples.push({
+        summary:
+          path
+            .parse(responseFilePath)
+            .name.replace(/^\d+-?/, '')
+            .replaceAll('-', ' ') || 'default',
+        value: JSON.parse(responseFile),
+      });
+    }
+
+    const RequestBodySchema = createRequestBodySchema(
+      schemaImport.name,
+      schemaImport.displayName,
+      schemaImport.VariablesSchema,
+      sha256Hash,
+    );
+
+    queries.push({
+      name: schemaImport.name,
+      displayName: schemaImport.displayName,
+      description: schemaImport.description,
+      tags: schemaImport.tags,
+      requestBodies,
+      responseExamples,
+      additionalSchemas,
+      VariablesSchema: schemaImport.VariablesSchema,
+      RequestBodySchema,
+      DataSchema: schemaImport.DataSchema,
+      ResponseSchema: schemaImport.ResponseSchema,
+    });
+  }
+
+  return queries;
+};
